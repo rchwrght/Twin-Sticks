0746c503dfcf705e00f0ae51e4d691f1a2e2d032
Rich Wright <rchwrght@gmail.com>
2017-10-15 16:07:00 +0200

    Creating First level with obstacles

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+
+    public GameObject player;
+
+    private float yTransform;
+
+	void Start () {
+        float offsetY = 8;
+	}
+	
+	
+	void Update () {
+        float playerPos = player.transform.position.z;
+        transform.position = new Vector3(transform.position.x, transform.position.y, playerPos);
+    }
+}
